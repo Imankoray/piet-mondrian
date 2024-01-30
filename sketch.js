815435262810c3f4c214b361d121f9b310a59212
Imankoray <146858173+Imankoray@users.noreply.github.com>
2024-01-30 14:43:27 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 function setup() {
 
-    const possibleColors = ["#f3f3f3", "#fbe317","#0c7fbf","#000000", "#f50f0f" ];
+    const possibleColors = ["#f3f3f3", "#fbe317", "#0c7fbf", "#000000", "#f50f0f"];
 
 
     //size of each square
@@ -10,58 +10,61 @@ function setup() {
     const cellNumber = 10;
     //canvas size in pixels
     const canvasSize = squareSize * cellNumber;
+    //list of possible cell sizes in pixels - based on square size 
+    const possibleCellSizes = [squareSize, 2 * squareSize, 3 * squareSize];
 
-const width = squareSize;
-const height = squareSize;
+    const width = squareSize;
+    const height = squareSize;
 
-        createCanvas(400, 400);
-        background(220);
+    createCanvas(400, 400);
+    background(220);
 
-        let x = 0;
-        let y = 0;
+    let x = 0;
+    let y = 0;
 
-        while(y < canvasSize) {
+    while (y < canvasSize) {
 
-        x=0
+          //generate a random width from the possible cell sizes
+          const actualHeight = random(possibleCellSizes);
 
-            while (x < canvasSize){
+        x = 0
 
-              
-                //choose random hex code from all the possible colours 
-                const colorCode = random(possibleColors);
-                //store color object in myColor
-                const myColor = color(colorCode);
-    
-                fill(myColor)
-    
-               
-    
-    
-                //set width and height
-                const width = squareSize;
-              const height = squareSize;
-    
-                
-    
-                //rect (x, y, width, height)
-                //x = position of the left of the square
-                //y - postiion of the top of the square
-                //s = size of the side of the square
-                 rect(x, y, width, height);
-    
-    //update x
-    x = x + width;
 
-    //update y
-    y= y + height;
-    
-            }
+        while (x < canvasSize) {
+
+
+            //choose random hex code from all the possible colours 
+            const colorCode = random(possibleColors);
+            //store color object in myColor
+            const myColor = color(colorCode);
+
+            fill(myColor)
+
+            const actualWidth = random(possibleCellSizes);
+
+          
+
+
+
+            //rect (x, y, width, height)
+            //x = position of the left of the square
+            //y - postiion of the top of the square
+            //s = size of the side of the square
+            rect(x, y, actualWidth, actualHeight);
+
+            //update x
+            x = x + actualWidth;
+
         }
+        
+        //update y
+        y = y + actualHeight;
     }
-      
-  
-    
-    function preload(){
-    
-      
-    }
+}
+
+
+
+function preload() {
+
+
+}
